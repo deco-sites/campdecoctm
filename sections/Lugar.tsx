42f0b8507bcbd033c86257f4c4db7daa89d13e36
Cleyton Mendes <cleyton.mendest@gmail.com>
2024-05-30 13:57:13 -0300

    tarefa 4

--- sections/Lugar.tsx
+++ sections/Lugar.tsx
@@ -1,17 +1,17 @@
 import { Temperature } from "apps/weather/loaders/temperature.ts";
 
-interface Props{
-    temperatura: Temperature | null
-    texto: string
+interface Props {
+  temperatura: Temperature | null;
+  texto: string;
 }
 
-const Lugar = ({temperatura, texto}:Props) => {
+const Lugar = ({ temperatura, texto }: Props) => {
   return (
     <div class="flex flex-col items-center justify-center py-4">
-        <h4 class="text-xl">Temperatura atual: {temperatura?.celsius}°C</h4>
-        <p class="text-base">{texto}</p>
+      <h4 class="text-xl">Temperatura atual: {temperatura?.celsius}°C</h4>
+      <p class="text-base">{texto}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Lugar
+export default Lugar;
