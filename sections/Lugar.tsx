6042cdaa1090a47e161b4587066957251af5545e
Cleyton Mendes <cleyton.mendest@gmail.com>
2024-05-30 13:54:57 -0300

    tarefa 2

---
+++ sections/Lugar.tsx
@@ -0,0 +1,17 @@
+import { Temperature } from "apps/weather/loaders/temperature.ts";
+
+interface Props{
+    temperatura: Temperature | null
+    texto: string
+}
+
+const Lugar = ({temperatura, texto}:Props) => {
+  return (
+    <div class="flex flex-col items-center justify-center py-4">
+        <h4 class="text-xl">Temperatura atual: {temperatura?.celsius}°C</h4>
+        <p class="text-base">{texto}</p>
+    </div>
+  )
+}
+
+export default Lugar
