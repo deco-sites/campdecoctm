3cd2e65117f9df131d041b68bd29f481623b56b2
Cleyton Mendes <cleyton.mendest@gmail.com>
2024-05-30 13:15:41 -0300

    feat: tarefa 1

--- sections/Cupom.tsx
+++ sections/Cupom.tsx
@@ -1,19 +1,19 @@
 import { HTMLWidget as HTML } from "apps/admin/widgets.ts";
 
 interface Props {
-    /** @title Código do Cupom */
-    codCupom: string
-    /** @title Descrição do Cupom */
-    description: HTML
-  }
-
-const Cupom = ({codCupom, description}:Props) => {
-    return (
-        <div class="flex items-center justify-center flex-col my-8">
-            <h3 class="text-3xl font-bold text-green-600">{codCupom}</h3>
-            <div dangerouslySetInnerHTML={{ __html: description }}/>
-        </div>
-    )
+  /** @title Código do Cupom */
+  codCupom: string;
+  /** @title Descrição do Cupom */
+  description: HTML;
 }
 
-export default Cupom
+const Cupom = ({ codCupom, description }: Props) => {
+  return (
+    <div class="flex items-center justify-center flex-col my-8">
+      <h3 class="text-3xl font-bold text-green-600">{codCupom}</h3>
+      <div dangerouslySetInnerHTML={{ __html: description }} />
+    </div>
+  );
+};
+
+export default Cupom;
